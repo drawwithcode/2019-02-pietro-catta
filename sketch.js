9dd9e9d41b9f3b23a38ec35ebc021053058d7522
pietro_catta <pietro4.cattaneo@mail.polimi.it>
2019-10-01 23:58:55 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
-function preload(){
-  // put preload code here
-}
+var colorList = ["#bccad6", "#8d9db6", "#667292", "#f1e3dd", "#fbefcc", "#f9ccac", "#f4a688", "#e0876a"]
+var colorElenco = ['#e86584', '#3c5979', '#018cb7', '#fae093'];
 
 function setup() {
-  // put setup code here
-}
+  createCanvas(windowWidth, windowHeight);
+  background("darkred");
 
-function draw() {
-  // put drawing code here
+  for (var x = 50; x < windowWidth - 50; x += 50) {
+    for (var y = 50; y < windowHeight - 100; y += 50) {
+      strokeWeight(5)
+      stroke("darkred");
+
+
+      if (x <= windowWidth / 2) {
+        var index = floor(random() * colorList.length);
+        var colorHex = colorList[index];
+        fill(color(colorHex));
+      } else if (x > windowWidth / 2) {
+        var index = floor(random() * colorElenco.length);
+        var colorHex = colorElenco[index];
+        fill(color(colorHex));
+      }
+
+      quad(x, y, x, y + 100, x + 50, y, x + 50, y + 100)
+    }
+  }
 }
